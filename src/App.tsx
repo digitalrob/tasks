90e008127f5fbcb07a2bf49f9546526a2f0c3a30
userName <robertjr@udel.edu>
2022-09-01 17:39:51 -0400

    added hello world

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                Robert Reardon UD CISC275 with React Hooks and TypeScript
+                UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <p>Robert Reardon says...</p>
+            <p>Hello World</p>
         </div>
     );
 }
